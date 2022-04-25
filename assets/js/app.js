be7411e5cbbbe835822f67e18227efd9f280e3e1
Daniel Wurring <103683060+reis-daniel@users.noreply.github.com>
2022-04-26 00:05:55 +0200

    function addnewtodo implemented

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,17 +1,22 @@
-// Variablen
-let userInput = document.querySelector(".userInput");
-let userInputValue = document.querySelector(".userInput").value;
 let addButton = document.querySelector(".addButton");
-let listTodo = document.querySelector(".todoList");
-let todo = document.createElement("li");
-let checkbox = document.createElement("input");
-
-// Wenn der userInput nicht leer ist und der Anwender auf Add klickt,
-// sollte dieser als neues List-Item in der listTodo hinterlegt werden.
 
 addButton.onclick = function addNewTodo() {
-  todo.appendChild(document.createTextNode(userInputValue));
-  todo.classList.add("list-group-item", "list-group-item-primary");
-  listTodo.append(todo);
-  userInput.value = "";
+  let userInput = document.querySelector(".userInput");
+  let listTodo = document.querySelector(".todoList");
+  let todo = document.createElement("li");
+  let checkbox = document.createElement("input");
+
+  if (userInput.value) {
+    todo.appendChild(checkbox);
+    checkbox.type = "checkbox";
+    checkbox.classList.add("form-check-input", "me-1");
+
+    todo.appendChild(document.createTextNode(userInput.value));
+    todo.classList.add("list-group-item", "list-group-item-primary");
+    listTodo.append(todo);
+
+    userInput.value = "";
+  } else {
+    window.alert("Bitte Todo eingeben!");
+  }
 };
